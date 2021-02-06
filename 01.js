88bd45bb6aa6f61dc548371d3a56bd4eefcf6764
DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-02-06 15:43:59 +0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  const text = "Hello David, welcome to LakeHub!";
+
+  return (
+    <>
+      <h1>Exercise 1</h1>
+
+      <div>{text}</div>
+    </>
+  );
+}
